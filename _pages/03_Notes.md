914944821cbbb91ea47be0ce5009c83b1b05347d
jesa06 <joselynanda@gmail.com>
2022-08-26 14:21:10 -0700

    quiz

---
+++ _pages/03_Notes.md
@@ -0,0 +1,36 @@
+# **Big Ideas/Terms**
+
+## Vocabulary
+- ### Terminal- where you run commands
+- ### Repository- where you store code/files (you can store anything in here)
+    - *Master branch*- original branch
+    - *Branch*- a version of the master branch
+- ### Compiler- takes your code and tries to translate it to tell u if u have errors, runs that command
+---
+## Actions
+- ### Kernel- way of communication between machine language and hardware
+- ### Push- send out changes from your local repository to remote branch
+- ### Pull- taking from remote branch to local repository
+
+---
+## Formatting
+- ### To add an image in markdown- src="{{site.baseurl}}/images/filename"
+---
+
+## Softwares & Applications
+- ### VSCode- Code Editor
+- ### Git- coordinating work among programmers to develop source code/source code management
+- ### Jupyter Notebook- to edit and run notebook documents
+- ### Bash- command-line interpreter or user shell used to interpret user commands
+---
+## Languages
+- ### Python
+- ### Java/Javascript
+---
+## Different File Types
+- ### .ipynb is Jupyter Notebook
+- ### .md is Markdown
+
+
+
+
---
+++ _posts/2022-08-25HellotoPythonquiz.py
@@ -0,0 +1,65 @@
+# Python Vocabulary Quiz
+
+import getpass, sys
+from tkinter import Y
+
+def question_and_answer(prompt):
+    print("Question: " + prompt)
+    msg = input()
+    print("Answer: " + msg)
+    
+def question_with_response(prompt):
+    print("Question: " + prompt)
+    msg = input()
+    return msg
+
+questions = 6
+correct = 0
+
+print('Hello, ' + getpass.getuser() + " running " + sys.executable)
+print("You will be asked " + str(questions) + " questions.")
+question_and_answer("Are you ready to take a test?")
+
+rsp = question_with_response("What command is used to include other functions that are developed?")
+if rsp == "import":
+    print(rsp + " is correct :)")
+    correct += 1
+else:
+    print(rsp + " is incorrect:(")
+
+rsp = question_with_response("What command in this example is used to evaluate a response?")
+if rsp == "if":
+    print(rsp + " is correct :)")
+    correct += 1
+else:
+    print(rsp + " is incorrect :(")
+
+rsp = question_with_response("Each 'if' command contains an '_________' to determine a true or false condition?")
+if rsp == "expression":
+    print(rsp + " is correct :)")
+    correct += 1
+else:
+    print(rsp + " is incorrect :(")
+
+rsp = question_with_response("To use Static Text, what is the command or function?") 
+if rsp == "print()":
+    print(rsp + "is correct :)")
+    correct += 1
+else:
+    print(rsp + "is incorrect :(")
+
+rsp = question_with_response("What is the key word that defines a function, in Python?")
+if rsp == "def":
+    print (rsp + " is correct :)")
+    correct += 1
+else:
+    print(rsp + " is incorrect :(")
+    
+rsp = question_with_response("What turns number into string?")
+if rsp == "function str()":
+    print (rsp + "is correct :)")
+    correct += 1
+else:
+    print(rsp + "is incorrect :(")
+
+print(getpass.getuser() + " great job! you scored " + str(correct) +"/" + str(questions))
